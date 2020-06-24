c68367beea60080f30e0cef2780dd0841bab0f2d
deankoch <deankoch@gmail.com>
2020-06-24 01:56:49 -0600

    removed example function, added first exported function

---
+++ R/datadir_bc.R
@@ -0,0 +1,60 @@
+#' Set local directory for storage of raster data files
+#'
+#' All files downloaded/created by the rasterbc package are written to the directory assigned by this function.
+#'
+#' By default this is the path returned by \code{rappdirs::user_data_dir}. With argument \code{select==TRUE}, a different
+#' directory can be specified: By default the user is prompted for this choice by an interactive file dialog. Alternatively it
+#' can be specified in \code{data.dir}.
+#'
+#' @param select A boolean indicating whether the directory should be automatically selected (default) or user-selected
+#' @param data.dir An (optional) character string, providing the absolute path to the storage directory
+#'
+#' @return character string, the absolute path to the selected storage directory
+#'
+#' @importFrom utils choose.dir
+#' @export
+#' @examples
+#' datadir_bc(TRUE, 'H:/')
+datadir_bc = function(select=FALSE, data.dir=NULL)
+{
+  # storage directory set automatically by rappdirs unless user sets select=TRUE
+  if(!select)
+  {
+    # rappdirs picks a sensible (platform-dependent) directory
+    data.dir = rappdirs::user_data_dir('rasterbc')
+
+  } else if (is.null(data.dir)) {
+
+    # if no data.dir supplied, prompt for one with GUI
+    # this is a snippet from stackexchange (https://tinyurl.com/y7mmsaw7)
+    caption = 'Select rasterbc data storage directory'
+    if (exists('utils::choose.dir')) {
+
+      # this function only available for Windows
+      utils::choose.dir(caption=caption)
+
+    } else {
+      # alternative for non-Windows users
+      tcltk::tk_choose.dir(caption=caption)
+    }
+
+  }
+
+  # create the directory as needed
+  print(paste('data storage path set to:', data.dir))
+  if(!dir.exists(data.dir))
+  {
+    dir.create(data.dir, recursive=TRUE)
+    print(paste('directory created'))
+
+  } else {
+
+    print(paste('directory exists'))
+    if(length(dir(data.dir, all.files=TRUE)) > 2)
+    {
+      warning('warning: this directory appears to be non-empty. Contents may be overwritten!')
+
+    }
+  }
+  return(data.dir)
+}
