8c2d20e78fa431a409f6c2721900bc56dcc3e5ca
deankoch <deankoch@gmail.com>
2020-06-29 23:29:27 -0600

    findblocks_bc seems to work now. check() upcoming

---
+++ R/listdata_bc.R
@@ -0,0 +1,169 @@
+#' Identify which data layers are available online, and (optionally) which have been downloaded already
+#'
+#' Prints a list of collection and variable names available through rasterbc, and (optionally) returns a nested list indicating
+#' which of these exist on disk already
+#'
+#' The layers available through this package are organized into "collections", corresponding to their original online source.
+#' Layers in a collection are further organized by variable name, and are uniquely identified by the character string "varname"
+#' (and, if applicable, the year): The syntax for the varname string is one of the following:
+#'
+#' <variable name> (for collections 'dem', 'bgcz', 'borders', 'gfc')
+#'
+#' <variable name>-<year> (for collections 'nfdb', 'cutblocks', 'pine', 'gfc')
+#'
+#' <variable name>-<species code>-<year> (for collection 'fids')
+#'
+#' To do: When arguments collection and/or varname are provided, check only for those (sub)collections, returning
+#' the corresponding (sub)list. This saves having to scan the entire data storage directory, which can be slow.
+#'
+#' @param collection character string, indicating the data collection to query
+#' @param varname character string, indicating the layer to query (see Details, below)
+#' @param year integer, indicating the year to query (see Details, below)
+#' @param verbose integer (0, 1, 2), indicating how much information about the files to print to the console
+#'
+#' @return A (named) nested list of boolean values, one for each file. This list has the same structure and naming scheme as
+#' 'rasterbc::metadata_bc', but with boolean entries instead of character strings. The entry indicates whether the file exists on
+#' disk in the local data storage directory specified by 'rasterbc::datadir_bc'.
+#' @export
+#' @examples
+#' listdata_bc()
+listdata_bc = function(collection=NULL, varname=NULL, year=NULL, verbose=1)
+{
+  # get the data storage directory...
+  data.dir = getOption('rasterbc.data.dir')
+  if(is.null(data.dir))
+  {
+    # ... or prompt to create one if it doesn't exist yet
+    stop('Data directory undefined. Set it using raster::datadir_bc()')
+  }
+
+  # check if a collection was specified
+  if(is.null(collection))
+  {
+    # recursive call to query all collections when "collection" argument is not assigned
+    return(lapply(setNames(nm=names(rasterbc::metadata_bc)), function(collection) listdata_bc(collection, varname, year, verbose)))
+
+  } else {
+
+    # check if the supplied collection argument has been misspelled before proceeding
+    collections = names(rasterbc::metadata_bc)
+    if(collection %in% collections)
+    {
+        # a valid collection string has been specified. Fetch all of its variable names
+        varnames = setNames(nm=names(rasterbc::metadata_bc[[collection]]$metadata$varnames))
+
+        # check if a varname argument has been supplied
+        if(is.null(varname))
+        {
+          # recursive call to query all variable names when "varname" argument is not assigned
+          return(lapply(varnames, function(varname) listdata_bc(collection, varname, year, verbose)))
+
+        } else {
+
+          # check if the supplied varname argument has been misspelled before proceeding
+          if(varname %in% varnames)
+          {
+            # check if this varname corresponds to a time-series
+            if(varname == rasterbc::metadata_bc[[collection]]$metadata$varnames[varname])
+            {
+              # case: varname is a one-time layer (no time-series, year argument ignored)
+              if(!is.null(year))
+              {
+                warning('year argument (', year, ') ignored for this layer')
+              }
+
+              # build list of filenames and boolean indicating whether they exist on disk
+              fnames = rasterbc::metadata_bc[[collection]]$fname$block[[varname]]
+              fnames.exist = setNames(file.exists(paste0(data.dir, fnames)), fnames)
+              printout.prefix = paste0('[', collection, '] ', sum(fnames.exist), '/', length(fnames))
+              printout.suffix = paste0(varname, ' blocks (storage: ', paste0(data.dir, collection), ')')
+              if(verbose > 0)
+              {
+                # printout indicating fraction of blocks downloaded
+                print(paste(printout.prefix, printout.suffix))
+              }
+
+              # return the booleans (with names indicating filepath)
+              return(fnames.exist)
+
+            } else {
+
+              # case: varname has multiple years. Fetch the vector of years for this variable
+              years.all = rasterbc::metadata_bc[[collection]]$source$years
+              idx.years = sapply(names(years.all), function(year) varname %in% names(rasterbc::metadata_bc[[collection]]$fname$block[[year]]))
+              years.lyr = years.all[idx.years]
+
+              # check if a year argument has been supplied
+              if(is.null(year))
+              {
+                # recursive call to query all years when "year" argument is not assigned
+                return(lapply(years.lyr, function(year) listdata_bc(collection, varname, year, verbose)))
+
+              } else {
+
+                # check if the year is available
+                if(year %in% years.lyr)
+                {
+                  # year argument is valid. Find its index among the other layers
+                  year.string = years.lyr[years.lyr==year]
+                  idx.year = names(rasterbc::metadata_bc[[collection]]$fname$block) == names(year.string)
+
+                  # build list of filenames and existence boolean
+                  fnames = rasterbc::metadata_bc[[collection]]$fname$block[[which(idx.year)]][[varname]]
+                  fnames.exist = setNames(file.exists(paste0(data.dir, fnames)), fnames)
+                  printout.prefix = paste0('[', year, ']:[', collection, ']: ', sum(fnames.exist), '/', length(fnames), ' ')
+                  printout.suffix = paste0(varname, ' blocks (storage: ', paste0(data.dir, collection, '/', year), ')')
+                  if(verbose > 0)
+                  {
+                    # printout indicating fraction of blocks downloaded
+                    print(paste0(printout.prefix, printout.suffix))
+                  }
+                  return(fnames.exist)
+
+                } else {
+
+                  # error when "year" argument is invalid
+                  error.msg = paste0('variable name "', varname, '" not found in collection "', collection, '"')
+                  suggestions.msg = paste('\nChoose one of:', paste(varnames, collapse=', '))
+                  stop(paste(error.msg, suggestions.msg))
+
+                }
+
+              }
+
+            }
+
+          } else {
+
+            # error when "varname" argument is invalid
+            error.msg = paste0('variable name "', varname, '" not found in collection "', collection, '"')
+            suggestions.msg = paste('\nChoose one of:', paste(varnames, collapse=', '))
+            stop(paste(error.msg, suggestions.msg))
+
+          }
+        }
+
+    } else {
+
+      # error when "collection" argument is invalid
+      error.msg = paste0('collection "', collection, '" not found.')
+      suggestions.msg = paste('\nChoose one of:', paste(collections, collapse=', '))
+      stop(paste(error.msg, suggestions.msg))
+    }
+
+
+  }
+
+
+
+
+  # collection = ifelse(is.null(collection), '', collection)
+  # year
+  # file.path(data.dir, collection)
+  #
+  #
+  # # check for NULL input
+  # rasterbc::metadata_bc$dem$fname
+  # list.files('H:\\git-MPB\\rasterbc_src', recursive=TRUE)
+
+}
---
+++ demo.R
@@ -0,0 +1,138 @@
+#' ---
+#' title: "demo.R"
+#' author: "Dean Koch"
+#' date: "June 19, 2020"
+#' output: github_document
+#' ---
+#'
+#' **Development version**: testing R code for fetching/caching/loading mapsheets
+
+#' Some preamble - I will need to organize this kind of data loading stuff into a function or RData data file for the package
+#'
+# load the package and set the data directory
+library(devtools)
+load_all() # replace with library(rasterbc) when finished development
+datadir_bc(select=TRUE, 'H:/rasterbc_data/')
+
+#' This path string is stored as an R option. View it using:
+print(getOption('rasterbc.data.dir'))
+
+#' To demonstrate this package we'll need a polygon covering a (relatively) small geographical extent in BC.
+#' Start by loading the `bcmaps` package and grabbing the polygons for the BC provincial boundary and the
+#' Central Okanagan Regional District
+example.name = 'Regional District of Central Okanagan'
+library(bcmaps)
+bc.bound.sf = bc_bound()
+districts.sf = regional_districts()
+example.sf = districts.sf[districts.sf$ADMIN_AREA_NAME==example.name, ]
+
+#' Now have a look at where the selected district lies in the province, with reference to the NTS/SNRC grid.
+#' We use `st_geometry` to drop the feature columns from the `sf` objects and keep only the geometries, which helps to de-clutter
+#' plots whenever we're just interested in the location(s) of something, and not the attributes attached to those locations.
+plot(st_geometry(ntspoly_bc), main=example.name, border='red')
+plot(st_geometry(bc.bound.sf), add=TRUE, col=adjustcolor('blue', alpha.f=0.2))
+plot(st_geometry(example.sf), add=TRUE, col=adjustcolor('yellow', alpha.f=0.5))
+#'  The `add=TRUE` argument tells `plot` to overlay the new shape(s) onto our initial plot (the NTS/SNRC grid in red), and
+#' the `border` and `col` arguments specify the outline and fill colours, respectively.
+#'
+#' The base function `adjustcolor` is a very handy one-liner for making opaque colours transparent, *eg.* here it makes the NTS/SNRC grid
+#' lines visible after overlaying the (filled) BC polygon.
+#'
+#' Looking closely, `example.sf` (in yellow) overlaps with three different NTS/SNRC mapsheet blocks. To fetch all of the raster data corresponding to
+#' this polygon, we will need to download three files with the appropriate mapsheet codes. These 4-character codes can be added as annotations on this plot
+#' by a simple `text` call, where `st_coordinates(st_centroid(st_geometry(ntspoly_bc)))` returns a matrix of coordinates.
+plot(st_geometry(ntspoly_bc), main=example.name, border='red')
+plot(st_geometry(bc.bound.sf), add=TRUE, col=adjustcolor('blue', alpha.f=0.2))
+plot(st_geometry(example.sf), add=TRUE, col=adjustcolor('yellow', alpha.f=0.5))
+text(st_coordinates(st_centroid(st_geometry(ntspoly_bc))), labels=ntspoly_bc$NTS_SNRC, cex=0.5)
+
+#' We see that the required mapsheets are coded as: `O92H`, `O82E`, and `O82L`. The `rasterbc::findblocks_bc` function finds the codes automatically:
+example.codes = findblocks_bc(example.sf)
+print(example.codes)
+
+#' `metadata_bc` contains a list of filenames associated with these blocks, *eg.* for the elevation (`dem`) layer we have the
+#' following three rasters:
+collection = 'dem'
+varname = 'dem'
+example.filenames = metadata_bc[[collection]]$fname$block[[varname]][example.codes]
+print(example.filenames)
+
+#' To download these files we append a URL prefix pointing to the hosting service (FRDR)
+example.urls = paste0(metadata_bc[[collection]]$frdr, example.filenames)
+print(example.urls)
+
+# define the destination paths, create the subdirectories, and download the files
+#+ eval=FALSE
+dir.create(paste0(getOption('rasterbc.data.dir'), 'blocks'), recursive=TRUE)
+dest.files = paste0(getOption('rasterbc.data.dir'), example.filenames)
+for(idx.file in 1:length(dest.files))
+{
+  download.file(example.urls[idx.file], dest.files[idx.file], mode='wb')
+}
+
+#' These can be merged together into a larger block that covers the `example.sf` polygon:
+# merge the blocks using an external (GDAL) mosaic call, via tempfile
+example.tempfile = paste0(tempfile(), '.tif')
+gdalUtils::mosaic_rasters(dest.files, dst_dataset=example.tempfile)
+
+# load the output, assign min/max stats and variable name
+example.tif = setMinMax(raster::raster(example.tempfile))
+names(example.tif) = varname
+plot(example.tif)
+
+# plot the result
+plot(example.tif, main=paste0(example.name, '\nDigital Elevation Model'), colNA='black')
+plot(st_geometry(example.sf), add=TRUE)
+plot(st_geometry(ntspoly_bc), add=TRUE, border='red')
+text(st_coordinates(st_centroid(st_geometry(ntspoly_bc))), labels=ntspoly_bc$NTS_SNRC, cex=0.5)
+
+#' From here it is straightforward to crop or clip the layer to the boundary of the polygon:
+
+# create a cropped version, plot the result
+example.cropped.tif = crop(example.tif, example.sf)
+plot(example.cropped.tif, main=paste0(example.name, '\nDigital Elevation Model'), colNA='black')
+plot(st_geometry(example.sf), add=TRUE)
+
+# create a clipped version, plot the result
+example.clipped.tif = raster::mask(example.cropped.tif, example.sf)
+plot(example.clipped.tif, main=paste0(example.name, '\nDigital Elevation Model'), colNA='black')
+
+
+#' The function `rasterbc::getdata_bc` does all of this automatically: specify the desired variables and a geographical extent
+#' (either as a vector of mapsheet codes, or an sfc object), and any required blocks will be downloaded into the directory specified
+#' by `rasterbc::datadir_bc`, combined, and returned as a `RasterLayer` object:
+
+### test code snippets for metadata prep
+
+x = listdata_bc()
+
+x = listdata_bc('dem', 'loss')
+
+
+
+#' When downloading a layer, you have the option of requesting the full BC extent, or supplying a polygon (sf object) that specifies
+#' a smaller geographical extent. eg. to get the elevation data for the example district, use this command
+#'
+
+
+
+
+
+#'
+# # load the borders info and shapefiles for mapsheets
+# cfg.borders = readRDS(here('data', 'borders.rds'))
+# ntspoly_bc = sf::st_read(cfg.borders$out$fname$shp['blocks'])
+# blocks.codes = cfg.borders$out$code
+#
+# # create the source data directory, metadata list and its path on disk
+# collection = 'fids'
+#
+#
+#
+
+
+#+ include=FALSE
+# Convert to markdown by running the following line (uncommented)...
+# rmarkdown::render(here('demo.R'), run_pandoc=FALSE, clean=TRUE)
+# ... or to html ...
+# rmarkdown::render(here('demo.R'))
